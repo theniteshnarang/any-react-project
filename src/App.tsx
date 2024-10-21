be03e9f3db30157bb654b9b02062f58eab66de0a
theniteshnarang <narangnitesh@ymail.com>
2024-10-21 13:26:43 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
+import { PropsWithChildren } from "react";
 import { Container } from "./layout";
 
+const fileDirectories = [
+  {
+    name: "assets",
+    childrenObj: [
+      {
+        name: "react.svg",
+      },
+    ],
+  },
+  {
+    name: "layout",
+    childrenObj: [
+      {
+        name: "Container",
+        childrenObj: [
+          {
+            name: "ContainerComponent.tsx",
+            childrenObj: [],
+          },
+        ],
+      },
+      {
+        name: "index.ts",
+      },
+    ],
+  },
+  {
+    name: "App.tsx",
+  },
+];
+
+interface FileComponentProps {
+  name: string;
+  childrenObj?: FileComponentProps[];
+}
+
+interface ComponentProps extends PropsWithChildren {
+  file: FileComponentProps;
+}
+const FileComponent = ({ file, children }: ComponentProps) => {
+  return (
+    <div>
+      {file.name}
+      {children && <div style={{ paddingLeft: "1rem" }}>{children}</div>}
+    </div>
+  );
+};
+
+const FileParentComponent = ({ file }: ComponentProps) => {
+  return (
+    <FileComponent file={file}>
+      {file.childrenObj &&
+        file.childrenObj.map((fileItem) => (
+          <FileParentComponent file={fileItem} />
+        ))}
+    </FileComponent>
+  );
+};
+
 function App() {
-  return <Container>Do something here</Container>;
+  return (
+    <Container>
+      {fileDirectories.map((file) => (
+        <FileParentComponent file={file} />
+      ))}
+    </Container>
+  );
 }
 
 export default App;
